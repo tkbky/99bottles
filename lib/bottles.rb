a040f5862b37376d933c51564396390957630bf5
KY <infcurious@gmail.com>
2017-01-05 16:33:06 +0800

    Pass the 5th test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,6 +3,9 @@ class Bottles
 
   def verse(x)
     case x
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" \
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
