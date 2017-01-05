c273e575d36ad67676f3e12eda5beac36f075a8a
KY <infcurious@gmail.com>
2017-01-05 16:31:28 +0800

    Pass the 4th test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,17 @@
 class Bottles
-
   def initialize; end
 
   def verse(x)
-    if x == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    case x
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" \
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{x} bottles of beer on the wall, #{x} bottles of beer.\n" +
-      "Take one down and pass it around, #{x - 1} bottles of beer on the wall.\n"
+      "#{x} bottles of beer on the wall, #{x} bottles of beer.\n" \
+        "Take one down and pass it around, #{x - 1} bottles of beer on the wall.\n"
     end
   end
-
 end
