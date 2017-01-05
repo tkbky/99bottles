4d4d95da79306cd0d030c914403d5a490fb94cfb
KY <infcurious@gmail.com>
2017-01-05 16:29:38 +0800

    Pass the 3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,8 +3,13 @@ class Bottles
   def initialize; end
 
   def verse(x)
-    "#{x} bottles of beer on the wall, #{x} bottles of beer.\n" +
-    "Take one down and pass it around, #{x - 1} bottles of beer on the wall.\n"
+    if x == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{x} bottles of beer on the wall, #{x} bottles of beer.\n" +
+      "Take one down and pass it around, #{x - 1} bottles of beer on the wall.\n"
+    end
   end
 
 end
