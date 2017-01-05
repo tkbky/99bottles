844df8899db78a7a6ee509a0f24bd58b93ba1c68
KY <infcurious@gmail.com>
2017-01-05 16:26:47 +0800

    Pass the 1st test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,7 +3,8 @@ class Bottles
   def initialize; end
 
   def verse(x)
-    # TODO: Sing!
+    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
   end
 
 end
